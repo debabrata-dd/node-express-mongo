a114cf96f3e908318b37d97c53b4e0a258625f50
Debabrata Das <debabrata.dd@gmail.com>
2020-12-02 11:45:51 +0530

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB Connected...");
+  }
+);
+
+//Import Routes
+const postsRoute = require("./routes/posts");
+app.use("/posts", postsRoute);
 
 app.get("/", (req, res) => {
-  res.send("Welcome!");
+  res.send("Welcome Home!");
 });
 
 app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const Post = require("../models/Posts");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post("/", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+module.exports = router;
